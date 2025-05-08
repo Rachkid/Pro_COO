ba9e1d4d860b95c7daf0112c75b7c6491733a926
Rachkid <abouzeidirachid97@gmail.com>
2025-05-08 14:52:43 +0100

    Create current_datetime.sh

---
+++ current_datetime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+date_heure=$(date "+%Y-%m-%d %H:%M:%S")
+echo "la date et l'heure actuelle : $date_heure"
+
+#la commande date est une fonction qui est utisée pour affichée:
+#%Y: une année à 4 chiffres;
+#%M: un mois à 2 chiffres;
+#%d: jours à 2 chiffres;
+#%H: Heure au format 24h;
+#%M: Minutes;
+#%S: Secondes.
